9567b6001716c4b6c2cc442955d9fb2096fe6f32
Francis Odhiambo2 <francisodhiambo2@gmail.com>
2022-06-29 00:48:06 +0300

    Changed

--- main.js
+++ main.js
@@ -9,11 +9,9 @@ document.querySelector('#date').innerHTML = date.toDateString();
     textarea.value = ''
     console.log(feed);
     for(let entry of feed.entries){
-        //create a list element
+        
         let li = document.createElement('li');
-        //add HTML content to list items
         li.innerHTML = `<h4><a href="${entry.link}">${entry.title}</a></h4>`;
-        //append HTML content to list 
         textarea.appendChild(li);
     }
   });
